e9a2013827b96e88d0359ac21457bf5f7e0cf92e
andrewbdesign <contact@andrewbdesign.com>
2019-10-05 22:09:55 +1000

    heroku setup

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db');
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
 connectDB();
 
@@ -17,4 +15,12 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/bill', require('./routes/api/bill'));
 app.use('/api/expense', require('./routes/api/expense'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
